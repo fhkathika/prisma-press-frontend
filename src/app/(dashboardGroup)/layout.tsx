02642bd9ad3281163dc5901d7be99e613ffd97a2
fhkathika <fhkathika057@gmail.com>
2026-09-06 22:28:21 +0600

    project settings

---
+++ src/app/(dashboardGroup)/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DashboadLayout = ({children}:{children:React.ReactNode}) => {
+  return (
+    <>
+    {children}
+    </>
+  )
+}
+
+export default DashboadLayout
---
+++ src/app/(publicGroup)/news/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NewsByIdPage() {
+  return (
+    <div>NewsByIdPage</div>
+  )
+}

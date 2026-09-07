4a9f548f8712f8872a16ae2633d5b00ef1b82593
fhkathika <fhkathika057@gmail>
2026-09-07 16:16:45 +0600

    initial settings

--- src/app/(publicGroup)/page.tsx
+++ src/app/(publicGroup)/page.tsx
@@ -1,9 +1,15 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div >
      next js
+
+     <Button 
+     size={"xs"}
+     variant={"destructive"}
+     >click</Button>
     </div>
   );
 }

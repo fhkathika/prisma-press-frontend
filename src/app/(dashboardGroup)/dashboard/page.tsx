1fb5eafee32c6e1ab4024ac683ae33fd8f87baf7
fhkathika <fhkathika057@gmail.com>
2026-09-19 23:07:09 +0600

    working on proxy.js

--- src/app/(dashboardGroup)/dashboard/page.tsx
+++ src/app/(dashboardGroup)/dashboard/page.tsx
@@ -1,7 +1,10 @@
+
 import React from 'react'
 
-export default function UserDashboardPage() {
+const UserDashboardPage = () => {
   return (
     <div>UserDashboardPage</div>
   )
 }
+
+export default UserDashboardPage
---
+++ src/proxy.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function proxy(request: NextRequest) {
+  const pathname=request.nextUrl.pathname
+  console.log(request.nextUrl,"request")
+  console.log(pathname,"pathname")
+  // return NextResponse.redirect(new URL('/', request.url))
+  return NextResponse.next()
+}
+ 
+// Alternatively, you can use a default export:
+// export default function proxy(request: NextRequest) { ... }
+ 
+export const config = {
+  // matcher: '/dashboard/:path*',
+  matcher: '/((?!api|_next/static|_next/image|.*\\.png$).*)',
+}
